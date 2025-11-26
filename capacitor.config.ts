104491cc9d2959d728e546153de44fb8880f4a25
Pedro <pedrobahia@souunisuam.com.br>
2025-11-26 13:59:20 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vivafit',
+  webDir: 'www'
+};
+
+export default config;
